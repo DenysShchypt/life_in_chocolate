e42b46b1c19a2bfc1c60f1a88805fbf453ded726
igrok1803444 <v618591@gmail.com>
2023-06-07 11:33:05 +0300

    add auto padding scrypt

--- src/js/auto-body-padding.js
+++ src/js/auto-body-padding.js
@@ -1,3 +1,8 @@
-  var header = document.querySelector('.header-container');
-    var headerHeight = header.offsetHeight;
-    document.body.style.paddingTop = headerHeight + 'px';
+document.addEventListener('DOMContentLoaded', autoPadding());
+window.addEventListener('resize', autoPadding);
+    function autoPadding() {
+        var header = document.querySelector('.header-container');
+        var headerHeight = header.offsetHeight;
+        document.body.style.paddingTop = headerHeight + 'px';
+    }
+
